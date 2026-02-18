ffc3b4ff52a49bd24b8af9c54442ffe49a9c9bd8
Tan Yi Peng <tanyipeng834@gmail.com>
2026-02-18 16:46:30 +0800

    working LRU cache without eviction

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -13,6 +13,7 @@ int main(int argc, char**argv)
     int e,s,b,opt;
     // this is the trace file input
     char * t;
+    cache_line_t **cache ;
     opterr=0;
     
     while(-1!=(opt=getopt(argc,argv,"hvs:E:b:t:")))
@@ -62,7 +63,14 @@ int main(int argc, char**argv)
     }
 
 
-    processFile(t);
+    
+    // pass in the number of lines in a set and number of sets.
+    cache = initCache(e,s);
+    processFile(t,cache,e,b,s);
+ 
+
+
+
 
     
 
@@ -89,13 +97,13 @@ void printHelp(char * prog){
 
 }
 
-void processFile(char* traceFilePath){
+void processFile(char* traceFilePath , cache_line_t** cache, int lines, int byteBits, int setBits){
     char op;
     unsigned long memoryAddress;
     size_t size;
 
     FILE * traceFile = fopen(traceFilePath,"r");
-    if (traceFile ==NULL){
+    if (!traceFile){
         perror("File does not exist");
         exit(1);
     }
@@ -104,10 +112,44 @@ void processFile(char* traceFilePath){
     {
 
         if(op =='I') continue; 
+
         
-        printf("%c\n",op);
-        printf("%lx\n",memoryAddress);
-        printf("%zu\n",size);
+       // The tag is to uniquely identify a cache line within a set.  
+       unsigned long  tag = getTag(memoryAddress,byteBits,setBits);
+       int  set = getSet(memoryAddress,byteBits,setBits);
+
+       char cacheBehaviorStr[100] ;
+       char* strP = cacheBehaviorStr;
+
+       
+       int cacheBehavior = updateCache(tag,set,cache,lines);
+
+      switch (cacheBehavior){
+
+            case 0 :
+                strP +=sprintf(strP,"miss");
+                break;
+            case 1 :
+                strP += sprintf(strP,"hit");
+                break;
+            case 2 :
+                strP += sprintf(strP,"miss eviction");
+                break;
+        
+
+      }
+
+      if (op=='M'){
+        strP+=sprintf(strP," hit");
+      }
+
+      // print to stdout
+
+      printf("%c %lx %zu %s\n",op,memoryAddress,size,cacheBehaviorStr);
+
+    
+
+
 
 
 
@@ -126,3 +168,79 @@ void processFile(char* traceFilePath){
 
 
 }
+
+
+cache_line_t ** initCache(int e , int s){
+    cache_line_t ** cache;
+    int sets = 1<<s;
+    cache = malloc(sets*sizeof (cache_line_t *));
+    if(!cache){perror("Malloc Failed"); exit(1);}
+    for (int i =0;i<sets;i++)
+    {
+        cache[i] = calloc(e,sizeof(cache_line_t));
+        if (!cache[i]){perror("Calloc Failed"); exit(1);}
+
+
+    }
+
+    return cache;
+
+    
+
+
+
+}
+
+unsigned long static inline getTag(unsigned long memoryAddress, int b, int s){
+    // logical shift to get the tag
+    return memoryAddress >>(b+s);
+}
+
+int static inline getSet(unsigned long memoryAddress, int b , int s){
+ 
+    return (memoryAddress >>(b)) & ((1UL<<s)-1);
+}
+
+// 4 possiblities miss , hit miss + eviction represented by number 0 , a and 2
+int updateCache(unsigned long tag, unsigned long set, cache_line_t ** cache, int noOfLines){
+
+        int invalidSetIndex =-1;
+        cache_line_t * cacheSet = cache[set];
+        for (int i =0; i<noOfLines;i++){
+            cache_line_t currentLine = cacheSet[i];
+            if(currentLine.tag == tag && currentLine.valid ==1) return 1;
+            if (currentLine.valid==0 && invalidSetIndex ==-1) invalidSetIndex =i;
+           
+        }
+
+        // replace with the first line but we will implement LRU later.
+
+        if(invalidSetIndex!=-1){
+            cache_line_t *invalidLine = &cacheSet[invalidSetIndex];
+            invalidLine->tag = tag;
+            invalidLine->valid =1;
+            return 0;
+
+
+        }
+       
+
+        // find the set with the LRU cache and evict it
+
+        return 2;
+
+
+
+    
+
+        
+
+            
+    }
+
+
+
+
+
+
+
--- cachelab-handout/csim.h
+++ cachelab-handout/csim.h
@@ -1,9 +1,27 @@
 
 #ifndef CSIM_H
 
+typedef struct cache_line
+{
+    unsigned long tag;
+    // 0 or 1 
+    int valid;
+} cache_line_t;
+
 void printHelp(char* program);
 
-void processFile(char* traceFile);
+void processFile(char* traceFilePath , cache_line_t** cache, int lines, int byteBits, int setBits);
+
+// S represents the number of sets and
+// E represents the number of lines in the set.
+cache_line_t** initCache(int S , int E);
+
+// get the set from the memory address
+
+
+unsigned long static inline getTag(unsigned long memoryAddress, int b, int s);
+int static inline getSet(unsigned long memoryAddress, int b , int s);
+int updateCache(unsigned long tag, unsigned long set, cache_line_t ** cache, int noOfLines);
 
 
 #endif
